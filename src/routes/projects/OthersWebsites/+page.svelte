72cd9e3e13fc69291c34a85197a72d378952f641
Ronan <ronan.etudes@gmail.com>
2023-03-03 20:50:23 +0100

    redirection to other websites modified

--- src/routes/projects/OthersWebsites/+page.svelte
+++ src/routes/projects/OthersWebsites/+page.svelte
@@ -1,16 +1,28 @@
 
 <h1 class = "text-center  text-7xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-emerald-200 to-cyan-500 mt-5 mb-5">Redirections to my webstites</h1>
 
+<div class = "grid justify-center m-10">
 
-
-<div class = "items-center card bg-opacity-60 rounded-2xl w-1/2 m-auto p-5 min-w-fit border">
-
-    <h1 class = " text-7xl  m-6" >A simple Three.js app </h1>
-    <a class = " "> Testing Three.js in a simple website. Discovering the technology</a>
-    <a href = https://me.ronantremoureux.fr class = "btn  m-6" target = "_blank" rel="noreferrer noopener">About it </a>
-
+    <p class = "text-xl "> 
+        These are some of my websites. I use them to test new technologies and to learn new things. These are not meant to be beautyfull.
+    </p>
 
 </div>
 
+<div class="divider w-20 m-auto mt-20 mb-20"></div> 
 
+<div class = "items-center card bg-opacity-60 rounded-2xl w-1/2 m-auto p-5 min-w-fit border ">
+    <h1 class = " text-7xl  m-6" >A simple Three.js app </h1>
+    <a class = " "> Testing Three.js in a simple website. Discovering the technology</a>
+    <a href = https://threejsapp.ronantremoureux.fr class = "btn  m-6" target = "_blank" rel="noreferrer noopener">Visit it</a>
+</div>
 
+<div class="divider w-20 m-auto mt-20 mb-20"></div> 
+
+<div class = "items-center card bg-opacity-60 rounded-2xl w-1/2 m-auto p-5 min-w-fit border">
+    <h1 class = " text-7xl  m-6" >Next.js app</h1>
+    <a class = " "> Testing Next.js in a simple website. Discovering the technology</a>
+    <a href = https://next.ronantremoureux.fr class = "btn  m-6" target = "_blank" rel="noreferrer noopener">Visit it </a>
+</div>
+
+<div class="divider w-20 m-auto mt-20 mb-20"></div> 
